5d76ae0b398eadca13bcfd4456d5e71fe5b51f9b
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-12-13 06:26:52 +0100

    Made queue async-safe using mutexes

--- pkg/collections/queue/queue.go
+++ pkg/collections/queue/queue.go
@@ -2,30 +2,38 @@ package queue
 
 import (
 	"errors"
+	"sync"
 )
 
 type ErrEmptyQueue error
 
 // TODO(SergeyCherepiuk): Should be unit-tested
 type Queue[T any] struct {
+	mu  sync.RWMutex
 	buf []T
 }
 
 func New[T any](capacity int) Queue[T] {
 	return Queue[T]{
-		buf: make([]T, capacity),
+		buf: make([]T, 0, capacity),
 	}
 }
 
-func (q Queue[T]) IsEmpty() bool {
+func (q *Queue[T]) IsEmpty() bool {
+	q.mu.RLock()
+	defer q.mu.RUnlock()
 	return len(q.buf) == 0
 }
 
-func (q Queue[T]) Size() int {
+func (q *Queue[T]) Size() int {
+	q.mu.RLock()
+	defer q.mu.RUnlock()
 	return len(q.buf)
 }
 
 func (q *Queue[T]) Enqueue(value T) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
 	q.buf = append(q.buf, value)
 }
 
@@ -33,6 +41,9 @@ func (q *Queue[T]) Dequeue() (T, error) {
 	if q.IsEmpty() {
 		return *new(T), ErrEmptyQueue(errors.New("queue is empty"))
 	}
+
+	q.mu.Lock()
+	defer q.mu.Unlock()
 
 	value := q.buf[0]
 	q.buf = q.buf[1:]
